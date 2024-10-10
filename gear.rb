d1f2d5f2a49258036f43df44881849ad8b31d8bd
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-10 17:17:22 +0100

    added two new variables to attr_reader and created a new method. rim and tire variables were added to attr_reader to allow for the creation of a new method which calcualtes the gear inches.

--- gear.rb
+++ gear.rb
@@ -1,16 +1,24 @@
-# create a class called gear that allows me to find the ratio between chainring/cog
+# add a method onto the gear class that allows me to calculate the gear inches to compare bicycles that differ in both gearing and wheel size
+#gear inches = wheel diameter * gear ratio
+#wheel diameter = rim diameter + twice tire diameter
 
 class Gear
-  attr_reader :chainring, :cog
-  def initialize(chainring,cog)
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring,cog,rim,tire)
     @chainring = chainring
     @cog       = cog
+    @rim       = rim
+    @tire      = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
 end
 
-  puts Gear.new(52,11).ratio
-  puts Gear.new(30,27).ratio
+  puts Gear.new(52,11,26,1.5).gear_inches
+  puts Gear.new(52,11,24,1.25).gear_inches
