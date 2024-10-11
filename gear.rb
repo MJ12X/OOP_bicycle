5798cc1528c42a531ee5149ed36e067af34f0385
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-11 13:25:46 +0100

    compacted the code. Made it so wheel can find its own diameter and is included in the gear class.

--- gear.rb
+++ gear.rb
@@ -1,12 +1,11 @@
 # create a method to calculate diameters from an array
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
   def initialize(chainring,cog,rim,tire)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire      = tire
+    @wheel     = Wheel.new(rim,tire)
   end
 
   def ratio
@@ -17,32 +16,12 @@ class Gear
     ratio * diameter
   end
 
-  def diameter
-    rim + (tire * 2)
-  end
-end
-
-class RevealingReferences
-  attr_reader :wheels
-  def initialize(data)
-  @wheels = wheelify(data)
-  end
-
-  def diameters
-    wheels.collect {|wheel| diameter(wheel)}
-  end
-
-  def diameter(wheel)
-    wheel.rim + (wheel.tire * 2)
-  end
-
-  Wheel = Struct.new(:rim, :tire)
-  def wheelify(data)
-    data.collect {|cell| Wheel.new(cell[0], cell[1])}
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
 
 
-  puts Gear.new(52,11,26,1.5).gear_inches
-  puts Gear.new(52,11,24,1.25).gear_inches
