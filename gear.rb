6170a4d084ecff93a3fea8db8fe1846bc92f001a
MJ12X <179878352+MJ12X@users.noreply.github.com>
2024-10-11 13:32:54 +0100

    created a new class. I created a wheel class to allow for single responsibility classes to allow for code to be easily changed.

--- gear.rb
+++ gear.rb
@@ -2,10 +2,10 @@
 
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring,cog,rim,tire)
+  def initialize(chainring,cog, wheel = nil)
     @chainring = chainring
     @cog       = cog
-    @wheel     = Wheel.new(rim,tire)
+    @wheel     = wheel
   end
 
   def ratio
@@ -13,15 +13,30 @@ class Gear
   end
 
   def gear_inches
-    ratio * diameter
+    ratio * wheel.diameter
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim,tire)
+    @tire = tire
+    @rim  = rim
   end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
   end
 end
 
 
-
+@wheel = Wheel.new(26,1.5)
+puts @wheel.circumference
+@wheel = Wheel.new(30,1.25)
+puts @wheel.circumference
+puts Gear.new(52,11,@wheel).gear_inches
